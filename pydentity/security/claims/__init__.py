16dbdabe2150b65d8df04e81f4cfe1d72f072b7f
udachin077 <pypi.udachin@yandex.ru>
2024-07-28 21:50:31 +0300

    Update claim ant test

--- pydentity/security/claims/__init__.py
+++ pydentity/security/claims/__init__.py
@@ -1,3 +1,2 @@
 from .claim_types import ClaimTypes
-from .claim_value_types import ClaimValueTypes
 from .claims import Claim, ClaimsIdentity, ClaimsPrincipal
--- pydentity/security/claims/claims.py
+++ pydentity/security/claims/claims.py
@@ -1,168 +1,98 @@
-import inspect
 from collections.abc import Iterable
-from typing import Any, Optional, overload, Generator
+from copy import deepcopy
+from inspect import isfunction
+from typing import Any, Generator, Final, overload, Literal
 
 from pydentity.exc import ArgumentNoneException
+from pydentity.security.claims.claim_types import ClaimTypes
 from pydentity.types import Predicate
-from pydentity.utils import is_none_or_empty
-from .claim_types import ClaimTypes
-from .claim_value_types import ClaimValueTypes
 
 
 class Claim:
-    def __init__(
-            self,
-            claim_type: str,
-            claim_value: str,
-            value_type: Optional[str] = None,
-            issuer: Optional[str] = None,
-            original_issuer: Optional[str] = None,
-            subject: Optional["ClaimsIdentity"] = None
-    ) -> None:
+    __slots__ = ('_type', '_value')
+
+    def __init__(self, claim_type: str, claim_value: Any):
         self._type = claim_type
         self._value = claim_value
-        self._value_type = ClaimValueTypes.String if is_none_or_empty(value_type) else value_type
-        self._issuer = ClaimsIdentity.DEFAULT_ISSUER if is_none_or_empty(issuer) else issuer
-        self._original_issuer = self._issuer if is_none_or_empty(original_issuer) else original_issuer
-        self._subject = subject
 
     @property
     def type(self) -> str:
         return self._type
 
     @property
-    def value(self) -> str:
+    def value(self) -> Any:
         return self._value
 
-    @property
-    def value_type(self) -> str:
-        return self._value_type
-
-    @property
-    def issuer(self) -> str:
-        return self._issuer
-
-    @property
-    def original_issuer(self) -> str:
-        return self._original_issuer
-
-    @property
-    def subject(self) -> Optional["ClaimsIdentity"]:
-        return self._subject
-
-    def clone(self, identity: Optional["ClaimsIdentity"] = None):
-        return Claim(
-            claim_type=self._type,
-            claim_value=self._value,
-            value_type=self._value_type,
-            issuer=self._issuer,
-            original_issuer=self._original_issuer,
-            subject=identity
-        )
-
-    def dump(self):
-        return {
-            "claim_type": self.type,
-            "claim_value": self.value,
-            "value_type": self.value_type,
-            "issuer": self.issuer,
-            "original_issuer": self.original_issuer
-        }
-
-    @staticmethod
-    def from_dict(data):
-        return Claim(
-            claim_type=data["claim_type"],
-            claim_value=data["claim_value"],
-            value_type=data["value_type"],
-            issuer=data["issuer"],
-            original_issuer=data["original_issuer"]
-        )
+    def clone(self):
+        return deepcopy(self)
 
     def __repr__(self) -> str:
-        return f"{self.type}:{self.value}"
+        return f'<{self.__class__.__name__} {self.type}:{self.value} at {id(self)}>'
+
+    def __eq__(self, other):
+        if not isinstance(other, Claim):
+            raise TypeError('the operand must be of type Claim')
+        if self.type == other.type and self.value == other.value:
+            return True
+        return False
 
 
 class ClaimsIdentity:
-    DEFAULT_ISSUER = "LOCAL AUTHORITY"
-    DEFAULT_NAME_CLAIM_TYPE = ClaimTypes.Name
-    DEFAULT_ROLE_CLAIM_TYPE = ClaimTypes.Role
+    __slots__ = ('_authentication_type', '_claims', '_name_claim_type', '_role_claim_type',)
+
+    DEFAULT_ISSUER: Final[str] = 'LOCAL AUTHORITY'
+    DEFAULT_NAME_CLAIM_TYPE: Final[str] = ClaimTypes.Name
+    DEFAULT_ROLE_CLAIM_TYPE: Final[str] = ClaimTypes.Role
 
     def __init__(
             self,
-            identity: Optional["ClaimsIdentity"] = None,
-            claims: Optional[Iterable[Claim]] = None,
-            authentication_type: Optional[str] = None,
-            name_type: Optional[str] = None,
-            role_type: Optional[str] = None
+            authentication_type: str | None = None,
+            *claims: Claim,
+            name_claim_type: str = ClaimTypes.Name,
+            role_claim_type: str = ClaimTypes.Role
     ):
-
-        if identity is not None and is_none_or_empty(authentication_type):
-            self._authentication_type = identity.authentication_type
-        else:
-            self._authentication_type = authentication_type
-
-        if not is_none_or_empty(name_type):
-            self._name_claim_type = name_type
-        elif identity is not None:
-            self._name_claim_type = identity._name_claim_type
-        else:
-            self._name_claim_type = ClaimsIdentity.DEFAULT_NAME_CLAIM_TYPE
-
-        if not is_none_or_empty(role_type):
-            self._role_claim_type = role_type
-        elif identity is not None:
-            self._role_claim_type = identity._name_claim_type
-        else:
-            self._role_claim_type = ClaimsIdentity.DEFAULT_ROLE_CLAIM_TYPE
-
-        self.__instance_claims: list[Claim] = []
-
-        if identity is not None:
-            self.add_claims(*identity.__instance_claims)
-
-        if claims is not None:
-            self.add_claims(*claims)
+        self._authentication_type = authentication_type
+        self._claims = list(claims) or []
+        self._name_claim_type = name_claim_type
+        self._role_claim_type = role_claim_type
 
     @property
-    def name(self) -> Optional[str]:
+    def name(self) -> str | None:
         return self.find_first_value(ClaimTypes.Name)
 
     @property
     def is_authenticated(self) -> bool:
-        return not is_none_or_empty(self._authentication_type)
+        return bool(self._authentication_type)
 
     @property
-    def authentication_type(self) -> Optional[str]:
+    def authentication_type(self) -> str | None:
         return self._authentication_type
 
     @property
-    def name_claim_type(self) -> Optional[str]:
+    def name_claim_type(self) -> str | None:
         return self._name_claim_type
 
     @property
-    def role_claim_type(self) -> Optional[str]:
+    def role_claim_type(self) -> str | None:
         return self._role_claim_type
 
     @property
     def claims(self) -> Generator[Claim, Any, None]:
-        for claim in self.__instance_claims:
+        for claim in self._claims:
             yield claim
 
     def add_claims(self, *claims: Claim):
-        for claim in claims:
-            if claim.subject is self:
-                self.__instance_claims.append(claim)
-            else:
-                self.__instance_claims.append(claim.clone(self))
+        if not claims:
+            raise ArgumentNoneException('claims')
+        self._claims.extend(claims)
 
     def remove_claim(self, claim: Claim) -> bool:
-        if claim is None:
+        if not claim:
             return False
 
-        for i, _claim in enumerate(self.__instance_claims):
-            if _claim is claim:
-                self.__instance_claims.pop(i)
+        for i, _claim in enumerate(self._claims):
+            if _claim == claim:
+                self._claims.pop(i)
                 return True
 
         return False
@@ -175,31 +105,31 @@ class ClaimsIdentity:
     def find_all(self, _match: Predicate[Claim]) -> Generator[Claim, Any, None]:
         ...
 
-    def find_all(self, *args) -> Generator[Claim, Any, None]:
-        if inspect.isfunction(args[0]):
-            _match: Predicate[Claim] = args[0]
+    def find_all(self, claim_type_or_match: str | Predicate[Claim]) -> Generator[Claim, Any, None]:
+        if isfunction(claim_type_or_match):
+            _match: Predicate[Claim] = claim_type_or_match
         else:
             def _match(x: Claim):
-                return x is not None and x.type == args[0]
+                return x and x.type == claim_type_or_match
 
         for claim in self.claims:
             if _match(claim):
                 yield claim
 
     @overload
-    def find_first(self, claim_type: str) -> Optional[Claim]:
+    def find_first(self, claim_type: str) -> Claim | None:
         ...
 
     @overload
-    def find_first(self, _match: Predicate[Claim]) -> Optional[Claim]:
+    def find_first(self, _match: Predicate[Claim]) -> Claim | None:
         ...
 
-    def find_first(self, *args) -> Optional[Claim]:
-        if inspect.isfunction(args[0]):
-            _match: Predicate[Claim] = args[0]
+    def find_first(self, claim_type_or_match: str | Predicate[Claim]) -> Claim | None:
+        if isfunction(claim_type_or_match):
+            _match: Predicate[Claim] = claim_type_or_match
         else:
             def _match(x: Claim):
-                return x is not None and x.type == args[0]
+                return x and x.type == claim_type_or_match
 
         for claim in self.claims:
             if _match(claim):
@@ -208,15 +138,15 @@ class ClaimsIdentity:
         return None
 
     @overload
-    def find_first_value(self, claim_type: str, /) -> Optional[str]:
+    def find_first_value(self, claim_type: str, /) -> str | None:
         ...
 
     @overload
-    def find_first_value(self, _match: Predicate[Claim], /) -> Optional[str]:
+    def find_first_value(self, _match: Predicate[Claim], /) -> str | None:
         ...
 
-    def find_first_value(self, *args) -> Optional[str]:
-        if claim := self.find_first(*args):
+    def find_first_value(self, claim_type_or_match: str | Predicate[Claim]) -> str | None:
+        if claim := self.find_first(claim_type_or_match):
             return claim.value
         return None
 
@@ -225,18 +155,19 @@ class ClaimsIdentity:
         ...
 
     @overload
-    def has_claim(self, claim_type: str, claim_value: str, /) -> bool:
+    def has_claim(self, claim_type: str, claim_value: Any, /) -> bool:
         ...
 
-    def has_claim(self, *args) -> bool:
-        if inspect.isfunction(args[0]):
-            _match: Predicate[Claim] = args[0]
+    def has_claim(self, claim_type_or_match: str | Predicate[Claim], claim_value: Any = None) -> bool:
+        if isfunction(claim_type_or_match):
+            _match: Predicate[Claim] = claim_type_or_match
         else:
-            _type, _value = args
+            _type = claim_type_or_match
+            _value = claim_value
 
             def _match(x: Claim):
                 return (
-                        x is not None and
+                        x and
                         x.type.casefold() == _type.casefold() and
                         x.value == _value
                 )
@@ -246,29 +177,6 @@ class ClaimsIdentity:
                 return True
 
         return False
-
-    def clone(self):
-        return ClaimsIdentity(identity=self)
-
-    def dump(self):
-        return {
-            "authentication_type": self.authentication_type,
-            "name_type": self._name_claim_type,
-            "role_type": self.role_claim_type,
-            "claims": [claim.dump() for claim in self.claims]
-        }
-
-    @staticmethod
-    def from_dict(data):
-        identity = ClaimsIdentity(
-            authentication_type=data["authentication_type"],
-            name_type=data["name_type"],
-            role_type=data["role_type"],
-        )
-        identity.add_claims(
-            *[Claim.from_dict(claim_dict) for claim_dict in data["claims"]]
-        )
-        return identity
 
 
 class ClaimsPrincipal:
@@ -288,9 +196,8 @@ class ClaimsPrincipal:
     @property
     def claims(self) -> Generator[Claim, Any, None]:
         for identity in self._identities:
-            if identity is not None:
-                for claim in identity.claims:
-                    yield claim
+            for claim in identity.claims:
+                yield claim
 
     @overload
     def find_all(self, claim_type: str, /) -> Generator[Claim, Any, None]:
@@ -300,50 +207,47 @@ class ClaimsPrincipal:
     def find_all(self, _match: Predicate[Claim], /) -> Generator[Claim, Any, None]:
         ...
 
-    def find_all(self, *args) -> Generator[Claim, Any, None]:
-        if inspect.isfunction(args[0]):
-            _match: Predicate[Claim] = args[0]
+    def find_all(self, claim_type_or_match: str | Predicate[Claim]) -> Generator[Claim, Any, None]:
+        if isfunction(claim_type_or_match):
+            _match: Predicate[Claim] = claim_type_or_match
         else:
             def _match(x: Claim):
-                return x is not None and x.type == args[0]
+                return x and x.type == claim_type_or_match
 
         for identity in self._identities:
-            if identity is not None:
-                for claim in identity.find_all(_match):
-                    yield claim
+            for claim in identity.find_all(_match):
+                yield claim
 
     @overload
-    def find_first(self, claim_type: str, /) -> Optional[Claim]:
+    def find_first(self, claim_type: str, /) -> Claim | None:
         ...
 
     @overload
-    def find_first(self, _match: Predicate[Claim], /) -> Optional[Claim]:
+    def find_first(self, _match: Predicate[Claim], /) -> Claim | None:
         ...
 
-    def find_first(self, *args) -> Optional[Claim]:
-        if inspect.isfunction(args[0]):
-            _match: Predicate[Claim] = args[0]
+    def find_first(self, claim_type_or_match: str | Predicate[Claim]) -> Claim | None:
+        if isfunction(claim_type_or_match):
+            _match: Predicate[Claim] = claim_type_or_match
         else:
             def _match(x):
-                return x is not None and x.type == args[0]
+                return x and x.type == claim_type_or_match
 
         for identity in self._identities:
-            claim = identity.find_first(_match)
-            if claim is not None:
+            if claim := identity.find_first(_match):
                 return claim
-
         return None
 
     @overload
-    def find_first_value(self, claim_type: str, /) -> Optional[str]:
+    def find_first_value(self, claim_type: str, /) -> str | None:
         ...
 
     @overload
-    def find_first_value(self, _match: Predicate[Claim], /) -> Optional[str]:
+    def find_first_value(self, _match: Predicate[Claim], /) -> str | None:
         ...
 
-    def find_first_value(self, *args) -> Optional[str]:
-        if claim := self.find_first(*args):
+    def find_first_value(self, claim_type_or_match: str | Predicate[Claim]) -> str | None:
+        if claim := self.find_first(claim_type_or_match):
             return claim.value
         return None
 
@@ -355,15 +259,16 @@ class ClaimsPrincipal:
     def has_claim(self, claim_type: str, claim_value: Any, /) -> bool:
         ...
 
-    def has_claim(self, *args) -> bool:
-        if inspect.isfunction(args[0]):
-            _match: Predicate[Claim] = args[0]
+    def has_claim(self, claim_type_or_match: str | Predicate[Claim], claim_value: Any = None) -> bool:
+        if isfunction(claim_type_or_match):
+            _match: Predicate[Claim] = claim_type_or_match
         else:
-            _type, _value = args
+            _type = claim_type_or_match
+            _value = claim_value
 
             def _match(x: Claim):
                 return (
-                        x is not None and
+                        x and
                         x.type.casefold() == _type.casefold() and
                         x.value == _value
                 )
@@ -371,55 +276,39 @@ class ClaimsPrincipal:
         for identity in self._identities:
             if identity.has_claim(_match):
                 return True
-
         return False
 
-    def select_primary_identity(self, identities: Iterable[ClaimsIdentity]) -> Optional[ClaimsIdentity]:  # noqa
-        if identities is None:
-            raise ArgumentNoneException("identities")
-
+    def select_primary_identity(self, identities: Iterable[ClaimsIdentity]) -> ClaimsIdentity | None:
+        if not identities:
+            raise ArgumentNoneException('identities')
         for identity in identities:
-            if identity is not None:
-                return identity
-
+            return identity
         return None
 
     def add_identities(self, *identities: ClaimsIdentity):
-        if identities is None:
-            raise ArgumentNoneException("identities")
+        if not identities:
+            raise ArgumentNoneException('identities')
         self._identities.extend(identities)
 
     def is_in_role(self, role: str) -> bool:
         for _identity in self._identities:
-            if _identity is not None:
-                if _identity.has_claim(_identity.role_claim_type, role):
-                    return True
-
-        return False
-
-    def is_in_all_roles(self, *roles: str) -> bool:
-        if not roles:
-            return False
-
-        for role in roles:
-            if not self.is_in_role(role):
-                return False
-
-        return True
-
-    def is_in_any_role(self, *roles: str):
-        if not roles:
-            return False
-
-        for role in roles:
-            if self.is_in_role(role):
+            if _identity.has_claim(_identity.role_claim_type, role):
                 return True
-
         return False
 
-    def dump(self) -> list[dict]:
-        return [i.dump() for i in self._identities]
+    def is_in_roles(self, *roles: str, mode: Literal['all', 'any'] = 'all') -> bool:
+        if not roles:
+            return False
 
-    @staticmethod
-    def from_list(data: list[dict]):
-        return ClaimsPrincipal(*[ClaimsIdentity.from_dict(identity_dict) for identity_dict in data])
+        if mode == 'all':
+            for role in roles:
+                if not self.is_in_role(role):
+                    return False
+            return True
+        elif mode == 'any':
+            for role in roles:
+                if self.is_in_role(role):
+                    return True
+            return False
+        else:
+            raise ValueError('mode')
