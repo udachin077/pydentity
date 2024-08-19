a4d5ac4a9026fdb530420a99122c0d6b4040fef2
udachin077 <pypi.udachin@yandex.ru>
2024-08-19 09:28:01 +0300

    add __all__

--- pydentity/token_provider.py
+++ pydentity/token_provider.py
@@ -12,7 +12,8 @@ if TYPE_CHECKING:
 __all__ = (
     'TotpSecurityStampBasedTokenProvider',
     'EmailTokenProvider',
-    'PhoneNumberTokenProvider'
+    'PhoneNumberTokenProvider',
+    'AuthenticatorTokenProvider',
 )
 
 
